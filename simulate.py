b27f3e22e6479f66fc8047b1442026447b3c6e0e
coled <cole.drozdeks@gmail.com>
2025-02-10 15:01:52 -0700

    Initial Commit

--- simulate.py
+++ simulate.py
@@ -1,5 +1,13 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+p.stepSimulation()
+
+for i in range(1000):
+    print(i)
+    time.sleep(1/60)
 
 p.disconnect()
