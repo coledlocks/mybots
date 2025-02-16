7a0e9e566e45264b49a05ee217e1692c0f57b58f
coled <cole.drozdeks@gmail.com>
2025-02-16 12:54:13 -0700

    5x5 cube towers

--- generate.py
+++ generate.py
@@ -1,25 +1,20 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
+# function to create a world
+def create_world():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[-3, 3, 0.5], size=[1, 1, 1])
+    pyrosim.End()
 
-start_length, start_width = 0, 0 # starting x, y location of cube
+# function to create a robot
+def create_robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[1, 0, 1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[2, 0, 1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[1, 1, 1])
+    pyrosim.End()
 
-# this function creates a tower of 10 cubes with each cube being 90% of the dimensions as the one below it
-def cube_tower(start_length, start_width):
-    length, width, height, total_height = 1, 1, 1, 1 # initial parameters of bottom cube
-    start_height = 0.5
-    for i in range(10):
-        pyrosim.Send_Cube(name="Box", pos=[start_length, start_width, start_height], size=[length, width, height])
-        length, width, height = 0.9 * length, 0.9 * width, 0.9 * height # decreasing the cube parameters by 10%
-        start_height = total_height + height / 2 # setting a new starting height so cube sits on previous cube
-        total_height += height
-
-# nested for loops placing all the towers
-for i in range(5):
-    for j in range(5):
-        cube_tower(start_length, start_width)
-        start_length += 1
-    start_width += 1
-    start_length = 0
-
-pyrosim.End()
+create_world()
+create_robot()
