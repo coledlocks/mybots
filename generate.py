bb6b46aabf5bdebdea169b9c6268d72403d5f2d0
coled <cole.drozdeks@gmail.com>
2025-02-16 11:52:19 -0700

    5x5 cube towers

--- generate.py
+++ generate.py
@@ -1,7 +1,25 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+start_length, start_width = 0, 0 # starting x, y location of cube
+
+# this function creates a tower of 10 cubes with each cube being 90% of the dimensions as the one below it
+def cube_tower(start_length, start_width):
+    length, width, height, total_height = 1, 1, 1, 1 # initial parameters of bottom cube
+    start_height = 0.5
+    for i in range(10):
+        pyrosim.Send_Cube(name="Box", pos=[start_length, start_width, start_height], size=[length, width, height])
+        length, width, height = 0.9 * length, 0.9 * width, 0.9 * height # decreasing the cube parameters by 10%
+        start_height = total_height + height / 2 # setting a new starting height so cube sits on previous cube
+        total_height += height
+
+# nested for loops placing all the towers
+for i in range(5):
+    for j in range(5):
+        cube_tower(start_length, start_width)
+        start_length += 1
+    start_width += 1
+    start_length = 0
 
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,15 +1,19 @@
 import pybullet as p
 import time
+import pybullet_data
+
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 #p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
-p.stepSimulation()
-
-p.loadSDF("box.sdf")
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
 
 for i in range(1000):
     print(i)
+    p.stepSimulation()
     time.sleep(1/60)
 
 p.disconnect()
