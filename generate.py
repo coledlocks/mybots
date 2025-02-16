7517b0bd9b78e19f93afd81a1b2a498afdbc94ee
coled <cole.drozdeks@gmail.com>
2025-02-16 12:54:46 -0700

    finished joints assignment

--- generate.py
+++ generate.py
@@ -9,7 +9,7 @@ def create_world():
 # function to create a robot
 def create_robot():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[1, 1, 1])
+    pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[1, 1, 1]) # absolute link
     pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[1, 0, 1])
     pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[1, 1, 1])
     pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[2, 0, 1])
