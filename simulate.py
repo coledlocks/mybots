03df89c05d0522ce8f53752e0c1943216642803d
coled <cole.drozdeks@gmail.com>
2025-02-17 12:07:15 -0700

    adding motors

--- simulate.py
+++ simulate.py
@@ -14,13 +14,35 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
-backLegSensorValues = np.zeros(100)
-frontLegSensorValues = np.zeros(100)
+backLegSensorValues = np.zeros(1000)
+frontLegSensorValues = np.zeros(1000)
 
-for i in range(100):
+for i in range(1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    pyrosim.Set_Motor_For_Joint(
+
+        bodyIndex=robotId,
+
+        jointName=b'Torso_BackLeg',
+
+        controlMode=p.POSITION_CONTROL,
+
+        targetPosition=-np.pi/4.0,
+
+        maxForce=500)
+    pyrosim.Set_Motor_For_Joint(
+
+        bodyIndex=robotId,
+
+        jointName=b'Torso_FrontLeg',
+
+        controlMode=p.POSITION_CONTROL,
+
+        targetPosition=np.pi / 4.0,
+
+        maxForce=500)
     time.sleep(1/60)
 
 p.disconnect()
