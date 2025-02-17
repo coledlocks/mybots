fda053dde149eefa8e68f35672bc3cec34255480
coled <cole.drozdeks@gmail.com>
2025-02-17 10:56:02 -0700

    saving backleg sensor values

--- simulate.py
+++ simulate.py
@@ -1,6 +1,8 @@
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import numpy as np
 
 
 physicsClient = p.connect(p.GUI)
@@ -11,10 +13,15 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotId)
+
+backLegSensorValues = np.zeros(1000)
 
 for i in range(1000):
-    print(i)
     p.stepSimulation()
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     time.sleep(1/60)
 
 p.disconnect()
+
+np.save('C:Users/coled/PycharmProjects/mybots/data/backLegSensorValues.npy', backLegSensorValues)
