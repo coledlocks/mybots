9b02fa9bb622654e3b856845ecd753a24969a515
coledlocks <cole.drozdeks@gmail.com>
2025-02-24 17:06:38 -0500

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,15 @@
+# file for storing constants
+
+# importing necessary packages
+import numpy as np
+
+# max time of simulation
+MAX_TIME = 1000
+
+# back and front leg values
+backLegAmplitude = np.pi/4
+backLegFrequency = 30
+backLegPhaseOffset = 0
+frontLegAmplitude = -np.pi/1.75
+frontLegFrequency = 0
+frontLegPhaseOffset = np.pi/3
