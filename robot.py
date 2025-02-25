80b99c64b299f7353e3e645ee1dc7fdf7a84b593
coledlocks <cole.drozdeks@gmail.com>
2025-02-24 23:20:31 -0500

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,5 +1,22 @@
+# importing packages
+from sensor import SENSOR
+from motor import MOTOR
+import pybullet as p
+
 class ROBOT:
 
   def __init__(self):
 
-    pass
+    self.sensors = {}
+    self.motors = {}
+    self.robotId = p.loadURDF("body.urdf")
+    pyrosim.Prepare_To_Simulate(robotId)
+    self.robot.Prepare_to_Sense()
+  def Prepare_to_Sense(self):
+    self.sensors = {}
+    for linkName in pyrosim.linkNamesToIndices:
+      self.sensors[linkName] = SENSOR(linkName)
+
+  def Sense(self):
+    for i in range(self.sensors):
+      self.sensors[i].Get_Values(t)
