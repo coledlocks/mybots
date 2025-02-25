8134f1006b7d76b57be70d484031eda47d5e1023
coledlocks <cole.drozdeks@gmail.com>
2025-02-24 23:20:40 -0500

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -3,17 +3,8 @@ import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy as np
+from simulation import SIMULATION
 import constants as c
-
-# setting up physics client
-physicsClient = p.connect(p.GUI)
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
-#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
-p.setGravity(0,0,-9.8)
-planeId = p.loadURDF("plane.urdf")
-robotId = p.loadURDF("body.urdf")
-p.loadSDF("world.sdf")
-pyrosim.Prepare_To_Simulate(robotId)
 
 backLegSensorValues = np.zeros(c.MAX_TIME)
 frontLegSensorValues = np.zeros(c.MAX_TIME)
@@ -30,35 +21,11 @@ frontLegAngles = frontLegMotorCommands * (np.pi/4.0)
 # np.save("C:/Users/coled/PycharmProjects/mybots/data/frontLegMotorCommands.npy", frontLegMotorCommands)
 # exit()
 
-for i in range(1000):
-    p.stepSimulation()
-    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    pyrosim.Set_Motor_For_Joint(
-
-        bodyIndex=robotId,
-
-        jointName=b'Torso_BackLeg',
-
-        controlMode=p.POSITION_CONTROL,
-
-        targetPosition=backLegMotorCommands[i],
-
-        maxForce=100)
-    pyrosim.Set_Motor_For_Joint(
-
-        bodyIndex=robotId,
-
-        jointName=b'Torso_FrontLeg',
-
-        controlMode=p.POSITION_CONTROL,
-
-        targetPosition=frontLegMotorCommands[i],
-
-        maxForce=100)
-    time.sleep(1/240)
-
 p.disconnect()
 
 np.save("C:/Users/coled/PycharmProjects/mybots/data/backLegSensorValues.npy", backLegSensorValues)
 np.save("C:/Users/coled/PycharmProjects/mybots/data/frontLegSensorValues.npy", frontLegSensorValues)
+
+simulation = SIMULATE()
+world = WORLD()
+robot = ROBOT()
