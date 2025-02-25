023f0db642532d1789b7f604ce395158badf5fc4
coledlocks <cole.drozdeks@gmail.com>
2025-02-24 23:20:26 -0500

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,5 +1,12 @@
+# importing packages
+import constants as c
+import numpy as np
+
 class SENSOR:
 
   def __init__(self):
+    self.linkName = linkeName
+    self.values = np.zeros(c.MAX_TIME)
 
-    pass
+  def Get_Value(self):
+    self.linkName[i] = pyrosim.Get_Touch_Sensor_Value_For_Link(self.linkName[i])
